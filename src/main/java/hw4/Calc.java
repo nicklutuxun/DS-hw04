327aa152e1903e9d641014c09dfe7c7fba74bb31
Nick Lu <tlu32@jhu.edu>
2022-03-07 00:03:45 -0500

    Basic function

--- src/main/java/hw4/Calc.java
+++ src/main/java/hw4/Calc.java
@@ -1,6 +1,8 @@
 package hw4;
 
 
+import java.util.Scanner;
+
 /**
  * A program for an RPN calculator that uses a stack.
  */
@@ -11,6 +13,51 @@ public final class Calc {
    * @param args Not used.
    */
   public static void main(String[] args) {
-    System.out.println("Hello World");
+    LinkedStack<Integer> stack = new LinkedStack<>();
+    Scanner inputScanner = new Scanner(System.in);
+    String input;
+    boolean end = false;
+    
+    while (true) {
+      input = inputScanner.nextLine();
+      Scanner commandScanner = new Scanner(input);
+      while (commandScanner.hasNext()) {
+        String command = commandScanner.next();
+        if (command.equals("!")) {
+          end = true;
+          break;
+        }
+        if (!validInt(command)) {
+          System.out.println("ERROR: bad token");
+          continue;
+        }
+        System.out.println(command);
+        
+      }
+      
+      if (end) {
+        break;
+      }
+      
+    }
+  }
+  
+  private static boolean validInt(String input) {
+    if(input.isEmpty()) return false;
+    
+    for (int i = 0; i < input.length(); i++) {
+      char cur = input.charAt(i);
+      if (i == 0 && cur == '-') {
+        if (input.length() == 1) {
+          return false;
+        } else {
+          continue;
+        }
+      }
+      if (!Character.isDigit(cur)) {
+        return false;
+      }
+    }
+    return true;
   }
 }

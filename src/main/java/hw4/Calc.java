1a862cf8eff495cf04659fc4c8d7ec2b9b8bfd51
Nick Lu <tlu32@jhu.edu>
2022-03-07 01:30:15 -0500

    Complete basic operations

--- src/main/java/hw4/Calc.java
+++ src/main/java/hw4/Calc.java
@@ -1,6 +1,5 @@
 package hw4;
 
-
 import java.util.Scanner;
 
 /**
@@ -23,16 +22,23 @@ public final class Calc {
       Scanner commandScanner = new Scanner(input);
       while (commandScanner.hasNext()) {
         String command = commandScanner.next();
-        if (command.equals("!")) {
+        if ("!".equals(command)) {
           end = true;
           break;
+        }
+        if (validOperator(command)) {
+          evalOperator(command, stack);
+          continue;
+        }
+        if (validSpecialCommand(command)) {
+          evalSpecialCommand(command, stack);
+          continue;
         }
         if (!validInt(command)) {
           System.out.println("ERROR: bad token");
           continue;
         }
-        System.out.println(command);
-        
+        stack.push(Integer.parseInt(command));
       }
       
       if (end) {
@@ -42,13 +48,71 @@ public final class Calc {
     }
   }
   
-  private static boolean validInt(String input) {
-    if(input.isEmpty()) return false;
+  private static void evalSpecialCommand(String command, LinkedStack<Integer> stack) {
+    switch (command) {
+      case "?":
+        System.out.println(stack.toString());
+        break;
+      case ".":
+        System.out.println(stack.top());
+        break;
+      default:
+        break;
+    }
+  }
+  
+  private static boolean validSpecialCommand(String command) {
+    String[] specialCommands = {"?", "."};
+    boolean valid = false;
+    for (int i = 0; i < 2; i++) {
+      if (command.equals(specialCommands[i])) {
+        valid = true;
+        break;
+      }
+    }
+    return valid;
+  }
+  
+  private static boolean validOperator(String command) {
+    String[] operators = {"+", "-", "*", "/", "%"};
+    boolean valid = false;
+    for (int i = 0; i < 5; i++) {
+      if (command.equals(operators[i])) {
+        valid = true;
+        break;
+      }
+    }
+    return valid;
+  }
+  
+  private static void evalOperator(String command, LinkedStack<Integer> stack) {
+    int op2 = stack.top();
+    stack.pop();
+    int op1 = stack.top();
+    stack.pop();
+  
+    if ("+".equals(command)) {
+      stack.push(op1 + op2);
+    } else if ("-".equals(command)) {
+      stack.push(op1 - op2);
+    } else if ("*".equals(command)) {
+      stack.push(op1 * op2);
+    } else if ("/".equals(command)) {
+      stack.push(op1 / op2);
+    } else if ("%".equals(command)) {
+      stack.push(op1 % op2);
+    }
+  }
+  
+  private static boolean validInt(String command) {
+    if (command.isEmpty()) {
+      return false;
+    }
     
-    for (int i = 0; i < input.length(); i++) {
-      char cur = input.charAt(i);
+    for (int i = 0; i < command.length(); i++) {
+      char cur = command.charAt(i);
       if (i == 0 && cur == '-') {
-        if (input.length() == 1) {
+        if (command.length() == 1) {
           return false;
         } else {
           continue;

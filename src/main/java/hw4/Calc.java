eeb39bf1154a4ff4d553f0379aafd182fe9d4e06
Nick Lu <tlu32@jhu.edu>
2022-03-06 21:27:06 -0500

    First commit

---
+++ src/main/java/hw4/Calc.java
@@ -0,0 +1,16 @@
+package hw4;
+
+
+/**
+ * A program for an RPN calculator that uses a stack.
+ */
+public final class Calc {
+  /**
+   * The main function.
+   *
+   * @param args Not used.
+   */
+  public static void main(String[] args) {
+    System.out.println("Hello World");
+  }
+}
---
+++ src/main/java/hw4/README.md
@@ -0,0 +1,5 @@
+# Discussion
+
+**Document all error conditions you determined and why they are error
+ conditions. Do this by including the inputs that you used to test your
+  program and what error conditions they exposed:**
